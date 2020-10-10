ef89fd24dee2cb651bd180ee9a4e459ef7fce6e9
Sobhit jadoun <65443674+Hackertrackersj@users.noreply.github.com>
2020-10-11 00:24:34 +0530

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,60 @@
+#colour section
+red='\033[1;31m'
+rset='\033[0m'
+grn='\033[1;32m'
+ylo='\033[1;33m'
+#script coding starts
+clear
+echo " "
+echo " "
+echo '
+
+                  _  _  ___  ___   __  ____  ___ 
+                  __    __                  __              __               
+/  |  /  |                /  |            /  |              
+$$ |  $$ |  ______    ____$$ |  ______   _$$ |_     ______  
+$$ |  $$ | /      \  /    $$ | /      \ / $$   |   /      \ 
+$$ |  $$ |/$$$$$$  |/$$$$$$$ | $$$$$$  |$$$$$$/   /$$$$$$  |
+$$ |  $$ |$$ |  $$ |$$ |  $$ | /    $$ |  $$ | __ $$    $$ |
+$$ \__$$ |$$ |__$$ |$$ \__$$ |/$$$$$$$ |  $$ |/  |$$$$$$$$/ 
+$$    $$/ $$    $$/ $$    $$ |$$    $$ |  $$  $$/ $$       |
+ $$$$$$/  $$$$$$$/   $$$$$$$/  $$$$$$$/    $$$$/   $$$$$$$/ 
+          $$ |                                              
+          $$ |                                              
+          $$/                                               v 1.0
+'|lolcat
+echo " "
+echo " "
+sleep 6.0
+clear
+echo -e "$red                         ChEcKiNg..>$rset"
+sleep 2.0
+clear
+echo -e "$red                         ChEcKiNg...>$rset"
+sleep 2.0
+clear
+echo -e "$red                         ChEcKiNg....>$rset"
+sleep 2.0
+clear
+echo -e "$red                         ChEcKiNg.....>$rset"
+sleep 2.0
+clear
+echo " "
+echo " "
+echo -e "$grn                updating the Nitro-allinone please wait$rset"
+sleep 2.0
+cd $HOME
+rm -rf Nitro-allinone
+git clone https://github.com/Hackertrackersj/Nitro-allinone
+cd Nitro-allinone
+chmod +x *
+clear
+echo " "
+echo -e "$grn               Nitro-allinone has been updated check it now...$rset"
+sleep 3.0
+echo " "
+cd $HOME
+cd Nitro-allinone
+./Nitro-allinone
+clear
+sleep 1.0
